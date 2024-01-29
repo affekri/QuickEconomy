32b33b622aba8befdbc5365c6c16d1ae98aea195
affekri <alfred@derfla.net>
2024-01-29 18:08:45 +0100

    Added InventoryType check

--- src/main/java/net/derfla/quickeconomy/listeners/inventoryClickListener.java
+++ src/main/java/net/derfla/quickeconomy/listeners/inventoryClickListener.java
@@ -9,6 +9,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 
 public class inventoryClickListener implements Listener {
 
@@ -17,17 +18,22 @@ public class inventoryClickListener implements Listener {
         // Filter out bad events
         if (event.getInventory() == null || event.getCurrentItem() == null) return;
 
+        InventoryType inventoryType = event.getClickedInventory().getType();
+
+
         // Check if the inventory is an instance from this plugin
         // Check if it is the BankInventory
         if (event.getInventory().getHolder() instanceof bankInventory) {
-            Boolean cancel = ((bankInventory) event.getInventory().getHolder()).trigger(event.getCurrentItem(), event.getSlot());
             event.setCancelled(true);
+            if (inventoryType != InventoryType.CHEST) return;
+            Boolean cancel = ((bankInventory) event.getInventory().getHolder()).trigger(event.getCurrentItem(), event.getSlot());
             return;
 
             // Check if it is the ShopInventory
         } else if (event.getInventory().getHolder() instanceof ShopInventory) {
-            Boolean buy = ((ShopInventory) event.getInventory().getHolder()).trigger(event.getCurrentItem(), event.getSlot());
             event.setCancelled(true);
+            if (inventoryType != InventoryType.CHEST) return;
+            Boolean buy = ((ShopInventory) event.getInventory().getHolder()).trigger(event.getCurrentItem(), event.getSlot());
             if (!buy) return;
             Player player = (Player) event.getWhoClicked();
             float cost = ShopInventory.getShopCost();

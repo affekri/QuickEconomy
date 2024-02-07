cc200f7593f807959c385eef8ad8e1525e3aff3b
affekri <alfred@derfla.net>
2024-02-07 17:06:57 +0100

    Updated helpMessage

--- src/main/java/net/derfla/quickeconomy/command/QuickeconomyCommand.java
+++ src/main/java/net/derfla/quickeconomy/command/QuickeconomyCommand.java
@@ -12,7 +12,7 @@ public class QuickeconomyCommand implements CommandExecutor {
     static Plugin plugin = Bukkit.getPluginManager().getPlugin("QuickEconomy");
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String string, @NotNull String[] strings) {
-        String helpMessage = "Use /bal or /balance to see your balance. Use /bal send, followed by an amount and another player, to send coins to that player. To create a shop place a sign on a chest. On the first line write [SHOP]. On the second line write the cost for the the items, such as 10 or 5.5. To deposit or withdraw coins, visit a bank!";
+        String helpMessage = "Use /bal or /balance to see your balance. Use /bal send, followed by an amount and another player, to send coins to that player. To create a shop place a sign on a chest. On the first line write [SHOP]. On the second line write the cost for the the items, such as 10 or 5.5, followed by a / and then what kind of shop it is. To deposit or withdraw coins, visit a bank!";
         String helpMessageOp = "As you are a server operator you have some more options. You can use /bal set:add:subtract to change the balance of any player. You can also create a bank, simply place down a sign and write [BANK] on the first line.";
         if (plugin.getConfig().contains("strings.helpMessage") && plugin.getConfig().getString("strings.helpMessage") != null) {
             helpMessage = plugin.getConfig().getString("strings.helpMessage");

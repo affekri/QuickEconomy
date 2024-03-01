4a431d0868df02648964b0b85cf57d20a69de104
affekri <alfred@derfla.net>
2024-03-01 18:44:44 +0100

    Added EventHandler

--- src/main/java/net/derfla/quickeconomy/listener/PlayerChangeSettingsListener.java
+++ src/main/java/net/derfla/quickeconomy/listener/PlayerChangeSettingsListener.java
@@ -2,10 +2,12 @@ package net.derfla.quickeconomy.listener;
 
 import com.destroystokyo.paper.event.player.PlayerClientOptionsChangeEvent;
 import net.derfla.quickeconomy.util.Translation;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class PlayerChangeSettingsListener implements Listener {
 
+    @EventHandler
     public void onPlayerChangeSettings(PlayerClientOptionsChangeEvent event) {
         Translation.init(event.getPlayer());
     }

0ddf6fe7f8a932241707362301d2c094ebd05703
fraing1 <frans.ingelstam@iths.se>
2024-09-21 11:47:46 +0200

    Update BalanceFile.java
    Minor changes

--- src/main/java/net/derfla/quickeconomy/file/BalanceFile.java
+++ src/main/java/net/derfla/quickeconomy/file/BalanceFile.java
@@ -6,7 +6,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
-import java.io.*
+import java.io.*;
 
 public class BalanceFile {
 
@@ -18,21 +18,28 @@ public class BalanceFile {
         // Setup logic, gets called when plugin is loaded
         file = new File(plugin.getDataFolder(), "balance.yml");
 
-        if(!file.exists()){
+        if (!file.exists()) {
             try {
-                file.createNewFile();
-                plugin.getLogger().info("Created new file: " + file.getName());
+                boolean created = file.createNewFile();
+                if (created) {
+                    plugin.getLogger().info("Created new file: " + file.getName());
+                } else {
+                    plugin.getLogger().warning("File already exists: " + file.getName());
+                }
             } catch (IOException e) {
-                // Just sends out a warning
+                // Log an error when file creation fails
+                plugin.getLogger().severe("Error creating file: " + file.getName());
                 e.printStackTrace();
-                plugin.getLogger().warning("Couldn't create file: " + file.getName());
             }
         }
+
         customFile = YamlConfiguration.loadConfiguration(file);
     }
 
-    public static FileConfiguration get(){
-        // Gets the file
+    public static FileConfiguration get() {
+        if (customFile == null) {
+            plugin.getLogger().severe("Custom file not initialized. Did you call setup()?");
+        }
         return customFile;
     }
 
@@ -46,9 +53,14 @@ public class BalanceFile {
         }
     }
 
-    public static void reload(){
+    public static void reload() {
         // Reloads the file
-        customFile = YamlConfiguration.loadConfiguration(file);
+        if (file.exists()) {
+            customFile = YamlConfiguration.loadConfiguration(file);
+            plugin.getLogger().info("File reloaded successfully: " + file.getName());
+        } else {
+            plugin.getLogger().warning("File does not exist, cannot reload: " + file.getName());
+        }
     }
 
     public static boolean delete() {

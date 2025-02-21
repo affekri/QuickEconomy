9b6533ff15ccdc202d54caabb4457e4c09dd4efe
affekri <derfla.ak@gmail.com>
2025-02-21 22:14:29 +0100

    Create new PlayerAccount object

---
+++ src/main/java/net/derfla/quickeconomy/model/PlayerAccount.java
@@ -0,0 +1,63 @@
+package net.derfla.quickeconomy.model;
+
+/**
+ * This object is used to store data about the players account at a faster place.
+ */
+public class PlayerAccount {
+
+    private double balance;
+    private double change;
+    private String name;
+    private String createdTime;
+
+    /**
+     * Used when creating a new PlayerAccount.
+     * @param name The player name
+     * @param balance The players balance. When creating new accounts, this should be 0.
+     * @param change The change in balance from when the player last left the game. When creating new accounts, this should be 0.
+     * @param createdTime Timestamp from the time of account creation.
+     */
+    public PlayerAccount(String name, double balance, double change, String createdTime) {
+        this.balance = balance;
+        this.change = change;
+        this.name = name;
+        this.createdTime = createdTime;
+    }
+
+    public double balance() {
+        return balance;
+    }
+
+    public double change() {
+        return change;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String createdTime() {
+        return createdTime;
+    }
+
+    public void balance(double balanceNew) {
+        this.balance = balanceNew;
+    }
+
+    public void change(double changeNew) {
+        this.change = changeNew;
+    }
+
+    public void name(String nameNew) {
+        this.name = nameNew;
+    }
+
+    /**
+     * Custom method. Returns the PlayerAccount in a more readable format. Does not include change.
+     * @return A string representation of the PlayerAccount object.
+     */
+    @Override
+    public String toString() {
+        return name + ": " + balance + " (Created: " + createdTime + ")";
+    }
+}
